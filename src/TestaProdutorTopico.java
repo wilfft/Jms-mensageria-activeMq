06aebd65e378ef68bdefe7e1d40b8fa90d6e5424
wilfft <68745408+wilfft@users.noreply.github.com>
2021-04-12 19:25:43 -0300

    Tópicos e assinaturas duráveis: criado consumir de topico

--- src/TestaProdutorTopico.java
+++ src/TestaProdutorTopico.java
@@ -1,2 +1,30 @@
+import javax.jms.*;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
 public class TestaProdutorTopico {
+
+    public static void main(String[] args) throws NamingException, JMSException {
+
+        InitialContext context = new InitialContext();
+        ConnectionFactory factory = (ConnectionFactory) context.lookup("ConnectionFactory");
+
+        Connection connection = factory.createConnection();
+        connection.start();
+
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+        Destination topico = (Destination) context.lookup("multa");
+
+        MessageProducer producer = session.createProducer(topico);
+
+
+            Message message = session.createTextMessage("<PEDIDO> <" + 1234 + ">");
+            producer.send(message);
+
+
+        session.close();
+        connection.close();
+        session.close();
+    }
 }

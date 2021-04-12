f9e589264b1cbf710e5852370893f806bbe665d8
wilfft <68745408+wilfft@users.noreply.github.com>
2021-04-12 18:37:25 -0300

    Tópicos e assinaturas duráveis

---
+++ src/TesteProdutor.java
@@ -0,0 +1,30 @@
+import javax.jms.*;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+public class TesteProdutor {
+
+    public static void main(String[] args) throws NamingException, JMSException {
+
+        InitialContext context = new InitialContext();
+        ConnectionFactory factory = (ConnectionFactory) context.lookup("ConnectionFactory");
+        Connection connection = factory.createConnection();
+
+        connection.start();
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+        Destination fila = (Destination) context.lookup("detran");
+
+        MessageProducer producer = session.createProducer(fila);
+
+        for (int i=0; i<100000; i++){
+            Message message = session.createTextMessage("<Pedido> numero 2: "+i);
+
+            producer.send(message);
+        }
+
+        session.close();
+        connection.close();
+        context.close();
+    }
+}
